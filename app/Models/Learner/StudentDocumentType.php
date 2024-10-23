bf82762a381907c4896d2eb2b01456d76299c515
Christiano Darren Iskandar <darrench.talenta@gmail.com>
2024-10-23 21:47:10 +0700

    Adding Tables for Student Personal Data

---
+++ app/Models/Learner/StudentDocumentType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Learner;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentDocumentType extends Model
+{
+    use HasFactory;
+    protected $dates = ['deleted_at'];
+
+}
