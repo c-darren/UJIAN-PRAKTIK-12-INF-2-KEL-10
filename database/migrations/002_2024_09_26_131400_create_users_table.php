62d1a4780f4c9725b806f93a8fb747488d03502b
Christiano Darren Iskandar <darrench.talenta@gmail.com>
2024-10-23 21:45:12 +0700

    Enhance users and roles table
    -Add Soft Delete as an Improvement
    - Delete column "name" in users table

--- database/migrations/002_2024_09_26_131400_create_users_table.php
+++ database/migrations/002_2024_09_26_131400_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            // $table->string('name');
             $table->string('username')->unique();
             $table->string('avatar')->nullable();
             $table->foreignId('role_id')->constrained(
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
