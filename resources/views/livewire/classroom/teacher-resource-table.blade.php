2ca88b5653c98f36a4a15b76db3bbd1065c092ed
Christiano Darren Iskandar <darrench.talenta@gmail.com>
2024-12-29 15:18:55 +0700

    fix: Update search placeholder in TeacherResourceTable for clarity

--- resources/views/livewire/classroom/teacher-resource-table.blade.php
+++ resources/views/livewire/classroom/teacher-resource-table.blade.php
@@ -36,7 +36,7 @@
                     id="default-search" 
                     x-ref="searchInput" 
                     class="block w-full p-4 pl-10 pr-28 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    placeholder="Search by Names or Codes"
+                    placeholder="Search by description or title"
                     wire:model.debounce.500ms="search"
                 />
                 <!-- Tombol Search -->
