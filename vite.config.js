5c584b7489d4b12d260a13fc44951d3e6a2d6b05
Christiano Darren Iskandar <darrench.talenta@gmail.com>
2025-02-09 16:27:46 +0700

    feat: Add Vercel Configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,      // For HMR
         hmr: {
-            host: '192.168.1.9',
+            host: '192.168.1.105',
             protocol: 'http',
             port: 5173
         },
@@ -17,7 +17,8 @@ export default defineConfig({
     // },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js'],
             output: ['public/css/app.css', 'public/js/'],
             refresh: true,
         }),
